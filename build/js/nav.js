01e8fb97d8f961271e42bb677fb236693607ad2c
Mateomelano <mateomelano2003@gmail.com>
2024-12-26 11:57:32 -0300

    Avances nav

--- build/js/nav.js
+++ build/js/nav.js
@@ -1,13 +1,8 @@
-const hamburger = document.querySelector('.hamburger');
-const sidebar = document.querySelector('.sidebar');
-const overlay = document.querySelector('.overlay');
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector('.hamburger');
+  const sidebar = document.querySelector('.sidebar');
 
-hamburger.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-  overlay.classList.toggle('active');
-});
-
-overlay.addEventListener('click', () => {
-  sidebar.classList.remove('active');
-  overlay.classList.remove('active');
+  hamburger.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+  });
 });
